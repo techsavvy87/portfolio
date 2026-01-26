12c05f15891d9196b9df333d89a0ea091cf0ac84
techsavvy59 <p.star.p@yandex.com>
2026-01-26 01:20:57 -0800

    change about image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+import laptopImg from "../../Assets/about.jpg";
 import Toolstack from "./Toolstack";
 
 function About() {
@@ -28,10 +27,20 @@ function About() {
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            style={{
+              paddingTop: "120px",
+              paddingBottom: "50px",
+            }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img
+              src={laptopImg}
+              style={{
+                borderRadius: "50%",
+              }}
+              alt="about"
+              className="img-fluid"
+            />
           </Col>
         </Row>
         <h1 className="project-heading">
@@ -44,7 +53,6 @@ function About() {
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
-        <Github />
       </Container>
     </Container>
   );
