a928fa46adfc31308393e7286689d39ca6b8f475
techsavvy59 <p.star.p@yandex.com>
2026-01-27 04:20:02 -0800

    change cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/DanielDoe.pdf";
+import pdf from "../../Assets/../Assets/Ryan_FullStack.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
